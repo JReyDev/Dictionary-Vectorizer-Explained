6d90a1550c8406144f6b9a1f14edcfc5a8ba3fe0
JReyDev <102834451+JReyDev@users.noreply.github.com>
2023-06-14 20:24:05 -0700

    Update C++ DictionaryVectorizer.cpp
    Class added

--- C++ DictionaryVectorizer.cpp
+++ C++ DictionaryVectorizer.cpp
@@ -9,89 +9,102 @@
 #include <Eigen/Sparse>
 
 
-//Create an unordered hashmap to store our features & the feature column index with num_features
-std::unordered_map<std::string, int> feature_indices_;
-int num_features_ = 0;
-
+//Sample Data
 std::vector<std::unordered_map<std::string, std::string>> data0 = {
     {{"color", "orange"}, {"size", "small"}},
     {{"color", "white"}, {"model", "v1.01"}},
     {{"color", "green"}, {"year", "2023"}}
 };
 
+class SampleHashVectorizer {
 
-void Fit(const std::vector<std::unordered_map<std::string, std::string>>& data) {
-    for (const auto& map : data) {
+public:
 
-        for (const auto& mapkey : map) {
+    // Create an unordered hashmap to store our features & the feature column index with num_features
 
-            std::cout << mapkey.first << " " << mapkey.second << std::endl;
+    std::unordered_map<std::string, int> feature_indices_;
 
-            const std::string& key = mapkey.first;
+    int num_features_ = 0;
 
-            auto search = feature_indices_.find(key);
+    void Fit(const std::vector<std::unordered_map<std::string, std::string>>& data) {
 
-            if (search != feature_indices_.end()) {
-                std::cout << "Key " << key << " exists as a feature" << std::endl;
-            }
-            else {
-                std::cout << "Key " << key << " does not exist as a feature. Adding...." << std::endl;
+        for (const auto& map : data) {
 
-                feature_indices_[key] = num_features_;
+            for (const auto& mapkey : map) {
 
-                num_features_++;
+                std::cout << mapkey.first << " " << mapkey.second << std::endl;
 
+                const std::string& key = mapkey.first;
+
+                auto search = feature_indices_.find(key);
+
+                if (search != feature_indices_.end()) {
+
+                    std::cout << "Key " << key << " exists as a feature" << std::endl;
+                }
+                else {
+
+                    std::cout << "Key " << key << " does not exist as a feature. Adding...." << std::endl;
+
+                    feature_indices_[key] = num_features_;
+
+                    num_features_++;
+
+                };
             };
         };
     };
-};
 
-void Transform(const std::vector<std::unordered_map<std::string, std::string>>& data) {
-    std::size_t num_samples = data.size();
+    void Transform(const std::vector<std::unordered_map<std::string, std::string>>& data) {
 
-    std::cout << "num_samples = " << num_samples << std::endl;
+        std::size_t num_samples = data.size();
 
-    Eigen::SparseMatrix<double> sparseMatrix(num_samples, num_features_);
+        std::cout << "num_samples = " << num_samples << std::endl;
 
-    std::cout << "Sparse Matrix: " << std::endl;
+        Eigen::SparseMatrix<double> sparseMatrix(num_samples, num_features_);
 
-    std::cout << sparseMatrix << std::endl;
+        std::cout << "Sparse Matrix: " << std::endl;
+
+        std::cout << sparseMatrix << std::endl;
 
 
-    int index_count = 0;
+        int index_count = 0;
 
-    for (const auto& hash : data) { 
+        for (const auto& hash : data) { 
 
-        std::cout << " Index Count:  " << index_count << std::endl;
+            std::cout << " Index Count:  " << index_count << std::endl;
 
-        index_count++;
+            index_count++;
 
-        for (const auto& hashmap : hash) {
+            for (const auto& hashmap : hash) {
 
-            const std::string& first = hashmap.first;
+                const std::string& first = hashmap.first;
 
-            std::cout << "[" << index_count - 1 << "," << feature_indices_[first] << "]" << std::endl;
+                std::cout << "[" << index_count - 1 << "," << feature_indices_[first] << "]" << std::endl;
 
-            sparseMatrix.insert(index_count - 1, feature_indices_[first]) = 1;
+                sparseMatrix.coeffRef(index_count - 1, feature_indices_[first]) = 1;
 
-        };
-
-        for (int i = 0; i < sparseMatrix.rows(); ++i) {
-
-            for (int j = 0; j < sparseMatrix.cols(); ++j) {
-
-                std::cout << sparseMatrix.coeff(i, j) << " ";
             };
-            std::cout << std::endl;
+
+            for (int i = 0; i < sparseMatrix.rows(); ++i) {
+
+                for (int j = 0; j < sparseMatrix.cols(); ++j) {
+
+                    std::cout << sparseMatrix.coeff(i, j) << " ";
+                };
+                std::cout << std::endl;
+                };
+            };
         };
     };
-};
 
 int main() {
 
-    Fit(data0);
+    SampleHashVectorizer HashVec;
 
-    Transform(data0);
+    HashVec.Fit(data0);
+
+    HashVec.Transform(data0);
 
 
     return 0;
